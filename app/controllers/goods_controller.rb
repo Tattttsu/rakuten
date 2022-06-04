539d83b9e816c72bf400d7c00eac223db65619c0
Ubuntu <ubuntu@ip-172-31-39-240.ap-northeast-3.compute.internal>
2022-06-04 06:30:18 +0000

    devise

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -3,6 +3,7 @@ class GoodsController < ApplicationController
   
   
   def search
-   @goods = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword]) # This returns Enamerable object
+   @goods = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword]).page(params[:page])
+   # This returns Enamerable object
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-  get 'rakuten_search' => 'goods#search'
-  
+  devise_for :users
+  root "goods#search"
+  get "/search",  to: 'goods#search'
+  get "/user", to: "user"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
